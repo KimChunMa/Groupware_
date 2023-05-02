6f32e048a9859200db510c31a30de0b0af24b4dc
DHK5664 <sunsinger5664@gmail.com>
2023-05-02 16:59:27 +0900

    230502 김동혁 boardDto , boardEntity 작업중

--- src/main/java/connect/web/domain/board/BoardDto.java
+++ src/main/java/connect/web/domain/board/BoardDto.java
@@ -1,4 +1,18 @@
 package connect.web.domain.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class BoardDto {
+    private int bno;
+    private String btitle;
+    private String bcontent;
+    private String bdate;
+    private int bview;
+
+    private int mno;
+    private int part_no;
 }
