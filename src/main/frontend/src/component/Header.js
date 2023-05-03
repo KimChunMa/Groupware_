bda5fceb9d0a5b86fad5530c9662f99743eca95e
SEONGBONG <ksbthyls@gmail.com>
2023-05-03 17:13:37 +0900

    2023-05-03 17:13

---
+++ src/main/frontend/src/component/Header.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function Header( props ) {
+    return (<>
+
+        {/*이경석 링크용*/}
+        <a href="/messenger"> 메신저 들어가기</a>
+
+    </>);
+}
--- src/main/frontend/src/component/Main.js
+++ src/main/frontend/src/component/Main.js
@@ -1,10 +1,11 @@
 import React , { useState , useEffect } from 'react';
 
+// --------------------------- //
+
 export default function Main( props ){
 
     return (<>
-        {/*이경석 링크용*/}
-        <a href="/messenger"> 메신저 들어가기</a>
+
         <div> 메인페이지 </div>
     </>);
 }
