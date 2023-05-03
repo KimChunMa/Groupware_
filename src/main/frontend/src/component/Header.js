cf0982d3707673649e5657d6ad70da2f48020f8f
SEONGBONG <ksbthyls@gmail.com>
2023-05-03 18:00:51 +0900

    2023-05-03 18:00

--- src/main/frontend/src/component/Header.js
+++ src/main/frontend/src/component/Header.js
@@ -1,8 +1,10 @@
 import react from 'react';
+import styles from './css/main/header.css';
 
 export default function Header( props ) {
     return (<>
 
+        <a href="/"> HOME </a>
         {/*이경석 링크용*/}
         <a href="/messenger"> 메신저 들어가기</a>
 
