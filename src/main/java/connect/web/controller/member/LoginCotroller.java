55e2cf6118c53197ca69305a1f0ceb0123fc31e9
SEONGBONG <ksbthyls@gmail.com>
2023-05-08 16:29:35 +0900

    2023-05-08 중간저장 세션 저장완료 출력은아직

--- src/main/java/connect/web/controller/member/LoginCotroller.java
+++ src/main/java/connect/web/controller/member/LoginCotroller.java
@@ -1,6 +1,6 @@
 package connect.web.controller.member;
 
-import connect.web.domain.member.LoginService;
+import connect.web.service.member.LoginService;
 import connect.web.domain.member.MemberDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/connect/web/service/member/LoginService.java
+++ src/main/java/connect/web/service/member/LoginService.java
@@ -1,9 +1,13 @@
-package connect.web.domain.member;
+package connect.web.service.member;
 
+import connect.web.domain.member.MemberDto;
+import connect.web.domain.member.MemberEntity;
+import connect.web.domain.member.MemberEntityRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @Slf4j
@@ -11,8 +15,9 @@ import java.util.Optional;
 public class LoginService {
 
     @Autowired MemberEntityRepository memberEntityRepository;
+    @Autowired HttpServletRequest request;
 
-    public boolean login( MemberDto memberDto ) {
+    public boolean login(MemberDto memberDto ) {
         // JS 로부터 데이터 들어오는 지 확인용
         log.info("Login Service : " + memberDto );
 
@@ -21,10 +26,9 @@ public class LoginService {
         if( optionalMemberEntity.isPresent() ){ // 아이디로 엔티티를 찾아 존재한다면
             // 찾은엔티티의 패스워드와 입력받은 패스워드가 일치한다면
             if( optionalMemberEntity.get().getMemberPwd().equals( memberDto.getMemberPwd() ) ) {
-
-                // 로그인 성공! 세션생성
-                
-                
+                // 로그인 성공, 세션생성
+                request.getSession().setAttribute("login" , optionalMemberEntity.get() );
+                log.info( "session : " + request.getSession().getAttribute( "login" ) );
                 return true;
             }
         }
