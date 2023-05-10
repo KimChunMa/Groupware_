7d16119186f59eebae799e8fb343992c1d5a9bfb
학생 <gksruf7894@naver.com>
2023-05-10 17:26:05 +0900

    [2023-05-10]백한결 취합

--- src/main/java/connect/web/service/approval/ApprovalService.java
+++ src/main/java/connect/web/service/approval/ApprovalService.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.swing.text.html.parser.Entity;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,10 +38,23 @@ public class ApprovalService {
     @Autowired
     HttpServletRequest request;
 
+    public MemberEntity getMember(){
+        String memberId =(String)request.getSession().getAttribute("login");
+        log.info("로그인세션확인");
+        log.info(memberId);
+
+        Optional<MemberEntity> memberEntityOptional = memberEntityRepository.findByMemberId( memberId );
+
+        if(memberEntityOptional.isPresent()) {
+            MemberEntity memberEntity = memberEntityOptional.get();
+            return memberEntity;
+        }
+        return null;
+    }
+
     //서류 등록
     @Transactional
     public boolean approvalWrite( ApprovalDto approvalDto){
-
 
         String memberId =(String)request.getSession().getAttribute("login");
             log.info("로그인세션확인");
@@ -52,15 +67,22 @@ public class ApprovalService {
             log.info(memberEntity+"");
 
             ApprovalEntity approvalEntity = approvalDto.toApprovalEntity();
+            log.info("엔티티화되었는지");
+            log.info(approvalEntity+"");
 
             approvalEntity.setMemberEntity(memberEntity);
+            log.info("memberentity도 approvalentity에 저장되었는지");
+            log.info(approvalEntity+"");
+            log.info(approvalEntity.getMemberEntity().getMemberId()+"");
+
 
             ApprovalEntity approvalEntity2 = approvalEntityRepository.save(approvalEntity);
+            log.info("memberentity2도 approvalentity에 저장되었는지");
+            log.info(approvalEntity2+"");
 
             if(approvalEntity2.getApprovalNo() >=1 ){
                 return true;
             }
-
 
         }
         return false;
@@ -68,25 +90,43 @@ public class ApprovalService {
 
 
     //서류 상태출력
+    //내가 쓴 서류 상태출력
     @Transactional
-    public List<ApprovalDto> approvalDtoList( int approvalNo ){
-        log.info("c approvalDtoList : " );
+    public List<ApprovalDto> approvalDtoList( ){
 
+        List<ApprovalDto>list = new ArrayList<>();
 
+        log.info("확인");
+        log.info(getMember().getMemberNo()+"ㅇㅀㅇㅀㅇㅀ");
 
-        List<ApprovalDto> list = new ArrayList<>();
-/*        ApprovalDtoList.forEach((e) -> {
-            list.add(new ApprovalDto(
-                    e.getApproval_no(),
-                    e.getApproval_writer(),
-                    e.getApproval_title(),
-                    e.getApproval_content(),
-                    e.getApproval_status(),
-                    e.getApproval_data()
-            ));
-        });*/
-                return list;
+        List<ApprovalEntity> approvalEntityList = approvalEntityRepository.findByMember( getMember().getMemberNo() );
 
-    } //출력 e
+        log.info("s approvalEntityList "+approvalEntityList+"");
+
+        approvalEntityList.forEach((o)->{
+            list.add(o.approvalDto());
+        });
+
+        log.info("s list "+list+"");
+
+        return list;
+    }
+
+    //전체출력함수
+/*    @Transactional
+    public List<ApprovalDto>totalApproval(){
+
+        List<ApprovalDto>Alllist = new ArrayList<>();
+
+        List<ApprovalEntity> approvalAllList = approvalEntityRepository.findAll();
+
+        log.info("s approvalAllList 모든 게시물출력"+approvalAllList);
+
+        approvalAllList.forEach((o)->{
+            Alllist.add(o.approvalDto());
+        });
+        return Alllist;
+    }*/
+
 
 }//class e
