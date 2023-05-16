384c7fd2af9ef417e7d8ca10cc0a6d2fe545dd41
SEONGBONG <ksbthyls@gmail.com>
2023-05-16 12:27:07 +0900

    2023-05-16 주소록 조금 작업

--- src/main/frontend/src/component/AddressBook/AddrMain.js
+++ src/main/frontend/src/component/AddressBook/AddrMain.js
@@ -1,10 +1,16 @@
 import React , { useState , useEffect , useRef } from 'react';
 import axios from 'axios';
 
+import { Container } from "@mui/material"
+
 export default function AddrMain( props ) {
 
 
     return (<>
-        주소록 메인페이지.
+        <Container>
+            <div>
+                <h3> 주소록 관리 </h3>
+            </div>
+        </Container>
     </>)
 }
