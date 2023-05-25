42f93a5df2f0f651be3a69b19419673a7a5d6d9a
DHK5664 <sunsinger5664@gmail.com>
2023-05-25 11:42:19 +0900

    230525 오전 11:42 김동혁 CSS 완료 , 댓글 부분 프론트 수정

--- src/main/frontend/src/component/board/ReplyList.js
+++ src/main/frontend/src/component/board/ReplyList.js
@@ -6,7 +6,7 @@ import styles from '../css/board/List.css'
 export default function ReplyList(props) {
 
     const [ login , setLogin ] = useState( JSON.parse( sessionStorage.getItem('login_token') ) )
-    const [ replies , setReplies ] = useState([])
+
 
     // [김동혁]댓글 작성 핸들러
     const onWriteHandler = ()=>{
@@ -19,6 +19,7 @@ export default function ReplyList(props) {
         console.log('수정할 댓글 번호'+replyNo);
         props.onReplyUpdate(document.querySelector('.replyUpdateContent'+replyNo).value,replyNo);
         document.querySelector('.replyUpdateContent'+replyNo).value = '';
+        document.querySelector('.upDelBox'+replyNo).style.display = 'none'
     }
     console.log( props );
 
@@ -27,36 +28,54 @@ export default function ReplyList(props) {
         console.log('삭제할 댓글 번호 : ' +replyNo);
         props.onReplyDelete(replyNo);
     }
+    // [김동혁] 수정,삭제칸 보이기
+    const upDelBoxLook=(e, replyNo)=>{
+        document.querySelector('.upDelBox'+replyNo).style.display == 'none' ?
+        document.querySelector('.upDelBox'+replyNo).style.display = 'block' :
+        document.querySelector('.upDelBox'+replyNo).style.display = 'none'
+
+    }
+
 
     return (<>
     <div className="replyContainer">
         <div className="replyInput">
-            <div className="replyContent">
-                <textarea className="replyContentInput" type="text"/>
+            <div className="replyContentArea">
+                <textarea className="replyContent" type="text"/>
             </div>
             <div>
                 <button className="viewReplyBtn" onClick={onWriteHandler}> 댓글작성 </button>
             </div>
         </div>
-        <h3> 댓글 목록 </h3>
+        <div className="replyArea">
                 {   props.replies.map( (r)=>{
-                        return (<div>
-                                    <div>
-                                        <span>{r.memberName}</span>
-                                        <span>{r.replyDate}</span>
+                        return (<div className="reply">
+                                    <div className="replyHeader">
+                                        <div className="replyWriter">{r.memberName}</div>
+                                        <div>{ (login != null && login.memberNo == r.memberNo) ?
+                                            <>
+                                                <button className="replyUpDel" onClick={(e) => upDelBoxLook(e, r.replyNo)}> ··· </button>
+                                            </> :
+                                            <></> }
+                                        </div>
                                     </div>
-                                        <span>{r.replyContent}</span>
-                                    <span>{ (login != null && login.memberNo == r.memberNo) ?
-                                        <>
-                                            <input className={ 'replyUpdateContent'+(r.replyNo) } type="text" />
-                                            <button onClick={(e)=>onDeleteHandler(e,r.replyNo)}>삭제</button>
-                                            <button onClick={(e)=>onUpdateHandler(e,r.replyNo)}>수정</button>
-                                        </> :
-                                        <></> }
-                                    </span>
+                                        <div>
+                                            <div className="replyDetail">
+                                                {r.replyContent}
+                                            </div>
+                                            <div className={"upDelBox"+(r.replyNo)} style={{display:'none'}}>
+                                                <input className={ 'replyUpdateContent'+(r.replyNo) } type="text" />
+                                                <button className="replyUpDelBtn" onClick={(e)=>onDeleteHandler(e,r.replyNo)}>삭제</button>
+                                                <button className="replyUpDelBtn" onClick={(e)=>onUpdateHandler(e,r.replyNo)}>수정</button>
+                                            </div>
+                                        </div>
+                                        <div className="replyDate">
+                                            {r.replyDate}
+                                        </div>
                                 </div>)
                     })
                 }
+            </div>
         </div>
     </>)
 }
